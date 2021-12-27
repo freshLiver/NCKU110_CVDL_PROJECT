72781e75c32e94204a16c1dcef849fc359fe2884
WengJY89079 <WengJY89079@gmail.com>
2021-12-27 16:17:00 +0800

    repair

--- light_cnn.py
+++ light_cnn.py
@@ -62,8 +62,8 @@ class maxout_fm(nn.Module):
         super(maxout_fm, self).__init__()
         self.out_channels = out_channels
         self.filter_split = nn.Conv2d(in_channels, out_channels * 2, kernel_size=kernel_size, stride=stride, padding=padding)
-        self.filter_halfs = [nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding) for i in range(2)]
-        self.batch_norms = [nn.BatchNorm2d(out_channels) for i in range(2)]
+        self.filter_halfs = nn.ModuleList([nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding) for i in range(2)])
+        self.batch_norms = nn.ModuleList([nn.BatchNorm2d(out_channels) for i in range(2)])
         self.leaky = nn.LeakyReLU()
 
     def forward(self, input):
